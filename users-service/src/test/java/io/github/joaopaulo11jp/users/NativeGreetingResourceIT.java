1d5c52bde13377018796992bf3353ece6d58d5c5
joaopaulo11jp <joaopaulo11jp@gmail.com>
2020-10-22 23:57:13 -0300

    Initial commit

---
+++ users-service/src/test/java/io/github/joaopaulo11jp/users/NativeGreetingResourceIT.java
@@ -0,0 +1,9 @@
+package io.github.joaopaulo11jp.users;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeGreetingResourceIT extends GreetingResourceTest {
+
+    // Execute the same tests but in native mode.
+}
